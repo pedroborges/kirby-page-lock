1bd5542c65ba4fe77cfdec701114721630a0ab9a
Patrik Illy <patrik.illy@gmail.com>
2018-01-24 15:07:45 +0100

    Czech translation

--- src/translations.php
+++ src/translations.php
@@ -6,5 +6,6 @@ return [
     "es_ES" => "{name} está editando esta página.",
     "fr"    => "{name} est en train d'éditer cette page.",
     "pt_BR" => "{name} está editando esta página.",
-    "pt_PT" => "{name} está editando esta página."
+    "pt_PT" => "{name} está editando esta página.",
+    "cs_CZ" => "{name} upravuje tuto stránku."
 ];
